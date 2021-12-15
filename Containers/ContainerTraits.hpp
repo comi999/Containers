e6b2ae5e613433ba9b25f2692c7ecbbe6aaa9ff5
Len <glizzyzbro@hotmail.com>
2021-12-15 21:20:04 +1100

    [SYNC]

--- Containers/ContainerTraits.hpp
+++ Containers/ContainerTraits.hpp
@@ -349,127 +349,159 @@ namespace ContainerTraits
         };
     }; // End private namespace
 
+    // Does the given type implement a "Size" or "size" function?
     template < typename Container >
     static constexpr bool IsSizeable = HasSize< Container >::Value;
 
+    // This will call the given types "Size" or "size" function.
     template < typename Container, typename = enable_if_t< IsSizeable< Container >, void > >
     static size_t Size( const Container& a_Container )
     {
         return GetSizeImpl< Container >::GetSize( a_Container );
     }
 
+    // Does the given type have a "Begin" or "begin" function?
     template < typename Container >
     static constexpr bool HasBegin = HasBeginImpl< Container >::Value;
 
+    // Does the given type have a "CBegin" or "cbegin" function?
     template < typename Container >
     static constexpr bool HasCBegin = HasCBeginImpl< Container >::Value;
 
+    // Does the given type have a "RBegin" or "rbegin" function?
     template < typename Container >
     static constexpr bool HasRBegin = HasRBeginImpl< Container >::Value;
 
+    // Does the given type have a "CRBegin" or "crbegin" function?
     template < typename Container >
     static constexpr bool HasCRBegin = HasCRBeginImpl< Container >::Value;
 
+    // Does the given type have a "End" or "end" function?
     template < typename Container >
     static constexpr bool HasEnd = HasEndImpl< Container >::Value;
 
+    // Does the given type have a "CEnd" or "cend" function?
     template < typename Container >
     static constexpr bool HasCEnd = HasCEndImpl< Container >::Value;
 
+    // Does the given type have a "REnd" or "rend" function?
     template < typename Container >
     static constexpr bool HasREnd = HasREndImpl< Container >::Value;
 
+    // Does the given type have a "CREnd" or "crend" function?
     template < typename Container >
     static constexpr bool HasCREnd = HasCREndImpl< Container >::Value;
 
+    // Does the given type have both "Begin"/"begin" and "End"/"end"?
     template < typename Container >
     static constexpr bool IsIterable = HasBegin< Container > && HasEnd< Container >;
 
+    // This will call the given types "Begin" or "begin" function.
     template < typename Container >
     static auto Begin( Container& a_Container )
     {
         return GetBeginImpl< Container >::GetBegin( a_Container );
     }
 
+    // This will call the given types "Begin" or "begin" function.
     template < typename Container >
     static auto Begin( const Container& a_Container )
     {
         return GetBeginImpl< Container >::GetBegin( a_Container );
     }
 
+    // This will call the given types "End" or "end" function.
     template < typename Container >
     static auto End( Container& a_Container )
     {
         return GetEndImpl< Container >::GetEnd( a_Container );
     }
 
+    // This will call the given types "End" or "end" function.
     template < typename Container >
     static auto End( const Container& a_Container )
     {
         return GetEndImpl< Container >::GetEnd( a_Container );
     }
 
+    // This will call the given types "RBegin" or "rbegin" function.
     template < typename Container >
     static auto RBegin( Container& a_Container )
     {
         return GetRBeginImpl< Container >::GetRBegin( a_Container );
     }
 
+    // This will call the given types "RBegin" or "rbegin" function.
     template < typename Container >
     static auto RBegin( const Container& a_Container )
     {
         return GetRBeginImpl< Container >::GetRBegin( a_Container );
     }
 
+    // This will call the given types "REnd" or "rend" function.
     template < typename Container >
     static auto REnd( Container& a_Container )
     {
         return GetREndImpl< Container >::GetREnd( a_Container );
     }
 
+    // This will call the given types "REnd" or "rend" function.
     template < typename Container >
     static auto REnd( const Container& a_Container )
     {
         return GetREndImpl< Container >::GetREnd( a_Container );
     }
 
+    // Will get the iterator of given container type.
     template < typename Container >
     using GetIter = typename GetIterImpl< Container >::Type;
 
+    // Will get the const iterator of given container type.
     template < typename Container >
     using GetCIter = typename GetCIterImpl< Container >::Type;
 
+    // Will get the reverse iterator of given container type.
     template < typename Container >
     using GetRIter = typename GetRIterImpl< Container >::Type;
 
+    // Will get the const reverse iterator of given container type.
     template < typename Container >
     using GetCRIter = typename GetCRIterImpl< Container >::Type;
 
+    // Will get the underlying type of a given iterator type.
     template < typename Iter >
     using GetIterType = typename GetIterTypeImpl< Iter >::Type;
 
+    // Will be enabled if the given iterator derefs into the given type.
     template < typename Iter, typename Type >
-    using EnableIfIterType = enable_if_t< is_same_v< GetIterType< Iter >, Type >, void >;
+    using EnableIfIterType = enable_if_t< is_same_v< GetIterType< Iter >, Type > >;
 
+    // Will be disabled if the given iterator derefs into the given type.
     template < typename Iter, typename Type >
-    using DisableIfIterType = enable_if_t< !is_same_v< GetIterType< Iter >, Type >, void >;
+    using DisableIfIterType = enable_if_t< !is_same_v< GetIterType< Iter >, Type > >;
 
+    // Will be enabled if the given container type has "Size" or "size" implemented.
     template < typename Container >
-    using EnableIfSizeable = enable_if_t< IsSizeable< Container >, void >;
+    using EnableIfSizeable = enable_if_t< IsSizeable< Container > >;
 
+    // Will be disabled if the given container type is sizeable.
     template < typename Container >
-    using DisableIfSizeable = enable_if_t< !IsSizeable< Container >, void >;
+    using DisableIfSizeable = enable_if_t< !IsSizeable< Container > >;
 
+    // Will be enabled if the given container type is iterable.
     template < typename Container >
-    using EnableIfIterable = enable_if_t< IsIterable< Container >, void >;
+    using EnableIfIterable = enable_if_t< IsIterable< Container > >;
 
+    // Will be disabled if the given container type is iterable.
     template < typename Container >
-    using DisableIfIterable = enable_if_t< !IsIterable< Container >, void >;
+    using DisableIfIterable = enable_if_t< !IsIterable< Container > >;
 
+    // Will be enabled if the given type is Sizeable, Iterable, and has a given underlying type.
     template < typename Container, typename Type >
-    using EnableIfContainer = enable_if_t< IsSizeable< Container > && IsIterable< Container > && is_same_v< GetIterType< GetIter< Container > >, Type >, void >;
+    using EnableIfContainer = enable_if_t< IsSizeable< Container > && IsIterable< Container > && is_same_v< GetIterType< GetIter< Container > >, Type > >;
 
+    // Will be disabled if the given type is Sizeable, Iterable, and has a given underlying type.
     template < typename Container, typename Type >
-    using DisableIfContainer = enable_if_t< !IsSizeable< Container > || !IsIterable< Container > || !is_same_v< GetIterType< GetIter< Container > >, Type >, void >;
+    using DisableIfContainer = enable_if_t< !IsSizeable< Container > || !IsIterable< Container > || !is_same_v< GetIterType< GetIter< Container > >, Type > >;
 
 };
